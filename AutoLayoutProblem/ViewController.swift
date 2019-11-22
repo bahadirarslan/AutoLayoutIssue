e37c369e60ee4bfc75d44dd8a2b739a0058adf5f
bahadir arslan <bahadirarslan@gmail.com>
2019-11-22 17:01:42 -0300

    Add files via upload

---
+++ AutoLayoutProblem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutProblem
+//
+//  Created by Bahadır ARSLAN on 22.11.2019.
+//  Copyright © 2019 Bahadır ARSLAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
